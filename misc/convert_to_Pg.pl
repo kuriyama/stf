1c0840d92ca5daf2800bcb5304b55bbe7dd5df75
Jun Kuriyama <kuriyama@s2factory.co.jp>
2012-01-02 23:42:48 +0900

    Treat created_at/updated_at columns same as MySQL behavior.

--- misc/convert_to_Pg.pl
+++ misc/convert_to_Pg.pl
@@ -19,18 +19,35 @@ s|UNIQUE KEY\(|UNIQUE(|g;
 s|TINYINT |SMALLINT |g;
 s|CREATE TABLE (\w+) SELECT |CREATE TABLE $1 AS SELECT |g;
 s|PRIMARY KEY id |PRIMARY KEY |g;
+s|created_at INT NOT NULL|created_at INTEGER NOT NULL default floor(extract(epoch from now()))|g;
+s|updated_at TIMESTAMP|updated_at TIMESTAMP default now()|g;
 
 s{CREATE TABLE (\w+) \((.*?)\);}{indexing($1, $2)}egs;
 
 print <<END;
-DROP TABLE IF EXISTS storage;
-DROP TABLE IF EXISTS bucket;
-DROP TABLE IF EXISTS object;
-DROP TABLE IF EXISTS deleted_object;
-DROP TABLE IF EXISTS deleted_bucket;
 DROP TABLE IF EXISTS entity;
+DROP TABLE IF EXISTS deleted_bucket;
+DROP TABLE IF EXISTS deleted_object;
+DROP TABLE IF EXISTS object;
+DROP TABLE IF EXISTS bucket;
+DROP TABLE IF EXISTS storage;
 END
 print $_, join("", @idx);
+
+print <<'END';
+CREATE OR REPLACE FUNCTION update_stamp() RETURNS trigger AS $update_stamp$
+  BEGIN
+    NEW.updated_at = current_timestamp;
+    RETURN NEW;
+  END;
+$update_stamp$ LANGUAGE plpgsql;
+END
+
+foreach (qw(storage bucket object entity)) {
+  print <<"END";
+CREATE TRIGGER $_\_stamp BEFORE INSERT OR UPDATE ON $_ FOR EACH ROW EXECUTE PROCEDURE update_stamp();
+END
+}
 
 # ============================================================
 sub indexing {
